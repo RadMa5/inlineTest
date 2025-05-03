7b2509fd1b3f00b93cebeeb628f96471bd1164a4
RadMa <13coolguy37@gmail.com>
2025-05-03 19:40:20 +0500

    Full functionality

---
+++ search.php
@@ -0,0 +1,41 @@
+<?php 
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'inline';
+
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $r = $_GET['search'];
+
+    if (isset($r)) {
+        $stmt = $conn->prepare("SELECT postId, name, body FROM commentaries WHERE name LIKE :search OR body LIKE :search");
+        $stmt->bindValue(':search', '%' . $r . '%');
+        $stmt->execute();
+        $commentaries = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } else {
+        $commentaries = [];
+    }
+
+    $posts = [];
+
+    foreach ($commentaries as $comment){
+        $postQuery = $conn->prepare("SELECT title FROM posts WHERE id = :postId");
+        $postQuery->bindValue(':postId', $comment['postId']);
+        $postQuery->execute();
+        $postTitle = $postQuery->fetch(PDO::FETCH_ASSOC);
+        $posts[] = [
+            'commentName' => $comment['name'],
+            'commentBody' => $comment['body'],
+            'postTitle' => $postTitle['title']
+        ];
+    }
+
+    $commString = json_encode($posts, JSON_UNESCAPED_UNICODE);
+    echo $commString;
+} catch (PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
